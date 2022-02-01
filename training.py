259d096f22f083d02689d4a1dc1ac846b0e217bf
TG <98741834+tgsamadesu@users.noreply.github.com>
2022-02-01 15:52:48 +0500

    Add files via upload

---
+++ training.py
@@ -0,0 +1,4 @@
+d1 = {"Manga":"Whimsical-Pictures", "Sugoi":"Amazing", "Arigato":"Thanks", "No-ho-n-jin":"Japanese"}
+print ("Ask a Japanese Meaning")
+n1 = input ("The meaning you were searching for is...")
+print (d1[n1])
